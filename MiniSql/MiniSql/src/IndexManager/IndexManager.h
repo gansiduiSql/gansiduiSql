31a721ca98e119bf34fdc74c2869cc519bc3dfb3
lucas95123 <1245554637@qq.com>
2015-10-17 23:33:03 +0800

    defined the interface of Index Manager

--- MiniSql/MiniSql/src/IndexManager/IndexManager.h
+++ MiniSql/MiniSql/src/IndexManager/IndexManager.h
@@ -3,7 +3,12 @@
 
 class IndexManager
 {
-
+public:
+	void createIndex(std::string indexName, std::string attributeName, std::string indexKey, 
+					TYPE keyValueType, string fileName, ADDRESS fisrtRecordOffset);
+	void dropIndex(std::string indexName); //delet/drop index indexfile and index in this function
+	void deleteValues(std::string indexName, std::list<Expression> expressions); 
+	void selectValues(std::string indexName, std::list<Expression> expressions, RECORDBUFFER recordBuffer);
 };
 
 #endif
