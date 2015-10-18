601255de1a386d9cf4d46eb40187d3933929caa4
JackWang822 <wang_kejie@foxmail.com>
2015-10-18 23:53:29 +0800

    add insert values in RecordManger.h

--- MiniSql/MiniSql/src/RecordManager/RecordManager.cpp
+++ MiniSql/MiniSql/src/RecordManager/RecordManager.cpp
@@ -1,4 +1,5 @@
 #include "RecordManager.h"
+#include <sstream>
 
 using namespace std;
 
@@ -12,10 +13,48 @@ RecordManager::~RecordManager()
 
 }
 
-void RecordManager::insertValues(const std::string& tableName, const list<std::string>& values)
+BYTE* toByte(string& value, TYPE type)
 {
-	int tail = *(int *)(bmPtr->fetchARecord(tableName, 0));
 	
+}
+
+void RecordManager::insertValues(const std::string& tableName, const list<std::string>& values, const Table& table)
+{
+	int tail = *((int *)(bmPtr->fetchARecord(tableName, 0)));
+	int length;
+	
+	BYTE* buffer = new BYTE[length];
+	int offset = 0;
+	vector<Data> tableVec = table.getTableVec();
+	list<string>::const_iterator it = values.cbegin();
+	for (auto iter : tableVec)
+	{
+		stringstream ss;
+		ss << *it;
+		int length = iter.getLength();
+		switch (iter.getType())
+		{
+		case INT:
+			int num;
+			ss >> num;
+			memcpy(buffer + offset, (BYTE*)(num), length);
+			break;
+		case CHAR:
+			BYTE* tmp = new BYTE[length];
+			ss >> tmp;
+			memcpy(buffer + offset, tmp, length);
+			break;
+		case FLOAT:
+			float num;
+			ss >> num;
+			memcpy(buffer + offset, (BYTE*)(num), length);
+			break;
+		default:
+			break;
+		}
+		offset += length;
+	}
+
 }
 
 void RecordManager::deleteValues(const std::string& tableName)
