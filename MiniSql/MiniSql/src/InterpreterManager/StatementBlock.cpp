d9bfd5d2d399322353820618b971c1ae67958244
tomstream <tomstream@qq.com>
2015-10-28 20:32:05 +0800

    improvent performance of CatalogManager...I feel very exhausted after long time coding.God bless me I can finish tommorrow

--- MiniSql/MiniSql/src/InterpreterManager/StatementBlock.cpp
+++ MiniSql/MiniSql/src/InterpreterManager/StatementBlock.cpp
@@ -1,7 +1,14 @@
 #include "StatementBlock.h"
+#include "../CatalogManager/CatalogManager.h"
+#include "../Exception.h"
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
+void CreateTableBlock::check()
+{
+}
 
 void CreateTableBlock::execute()
 {
@@ -25,6 +32,13 @@ void CreateIndexBlock::execute()
 {
 }
 
+void CreateIndexBlock::check()
+{
+	auto pcb = CatalogManager::getCatalogManager();
+	if (pcb->isIndexExist(attributeName))
+		throw CatalogError("index(" + indexName + ") exists");
+}
+
 void CreateIndexBlock::print()
 {
 	cout << "indexName:" << indexName
@@ -35,6 +49,29 @@ void CreateIndexBlock::print()
 
 void InsertTableBlock::execute()
 {
+}
+
+void InsertTableBlock::check()
+{
+	auto pcb = CatalogManager::getCatalogManager();
+	auto table = pcb->getTable(tableName);
+	if (table.getTableName() != tableName)
+		throw CatalogError("Table name(" + tableName + ") does not exist");
+	auto& tableVec = table.getTableVec();
+	if (tableVec.size() != values.size())
+		throw CatalogError("The number of attributes does not match");
+	CheckType ct;
+	string s;
+	int size = tableVec.size();
+	for (int i = 0; i < size; i++) {
+		TYPE type = tableVec[i].getType();
+		if (!ct.isType(values[i], type)) {
+			throw CatalogError("The type does not match");
+			if (type == CHAR && values[i].length() == tableVec[i].getLength() + 2) {
+				values[i] = string(values[i].begin() + 1, values[i].end() - 1);
+			}throw CatalogError("The length of string does not match");
+		}
+	}
 }
 
 void InsertTableBlock::print()
@@ -50,6 +87,11 @@ void QuitBlock::execute()
 {
 }
 
+void QuitBlock::check()
+{
+	//no need to check
+}
+
 void QuitBlock::print()
 {
 	cout << "quit" << endl;
@@ -57,6 +99,13 @@ void QuitBlock::print()
 
 void DropTableBlock::execute()
 {
+}
+
+void DropTableBlock::check()
+{
+	auto pcb = CatalogManager::getCatalogManager();
+	if (!pcb->isTableExist(tableName))
+		throw CatalogError("The table does not exist");
 }
 
 void DropTableBlock::print()
@@ -68,6 +117,13 @@ void DropIndexBlock::execute()
 {
 }
 
+void DropIndexBlock::check()
+{
+	auto pcb = CatalogManager::getCatalogManager();
+	if (!pcb->isIndexExist(indexName))
+		throw CatalogError("The index does not exist");
+}
+
 void DropIndexBlock::print()
 {
 	cout << "drop " << indexName << endl;
@@ -76,6 +132,11 @@ void DropIndexBlock::print()
 void DeleteBlock::execute()
 {
 	
+}
+
+void DeleteBlock::check()
+{
+
 }
 
 void DeleteBlock::print()
@@ -95,6 +156,10 @@ void SelectBlock::execute()
 {
 }
 
+void SelectBlock::check()
+{
+}
+
 void SelectBlock::print()
 {
 	cout << "select ";
@@ -111,4 +176,46 @@ void SelectBlock::print()
 		}
 	}
 	cout << endl;
+}
+
+bool CheckType::isType(const std::string & s, TYPE type) {
+	switch (type) {
+	case INT:
+		return isInt(s);
+		break;
+	case FLOAT:
+		return isFloat(s);
+		break;
+	case CHAR:
+		return isString(s);
+		break;
+	default:return false;
+	}
+}
+
+bool CheckType::isString(const std::string & s) {
+	return *(s.begin()) == '\''&&*(s.end() - 1) == '\'';
+}
+
+bool CheckType::isFloat(const std::string & s) {
+	try {
+		stof(s);
+	}
+	catch (exception& e) {
+		return false;
+	}return true;
+}
+
+bool CheckType::isInt(const std::string & s) {
+	try {
+		stoi(s);
+	}
+	catch (exception& e) {
+		return false;
+	}return true;
+}
+
+bool CheckType::isAttribute(const std::string & s)
+{
+	return false;
 }
